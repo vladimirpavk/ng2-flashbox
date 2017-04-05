15617d991e1e1815fd80dc360edde1d2b941043a
Vladimir Pavković <vladimirpavk@gmail.com>
2017-04-05 15:09:48 +0200

    Working module...

--- commoncomponents.module.ts
+++ commoncomponents.module.ts
@@ -1,3 +1,4 @@
+/// <reference path="../../../../node_modules/@angular/common/index.d.ts" />
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlashBoxComponent }   from './components/flashbox/flashbox.component';
